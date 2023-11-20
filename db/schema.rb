5ff875a05a1db9b3aa337289cc33592dd765adc2
kitedesigner <133687852+kitedesigner@users.noreply.github.com>
2023-11-20 06:56:03 -0800

    Add files via upload

--- db/schema.rb
+++ db/schema.rb
@@ -2,49 +2,73 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
- #
- # It's strongly recommended that you check this file into your version control system.
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
 
- ActiveRecord::Schema.define(version: 2023_07_08_093645) do
+ActiveRecord::Schema.define(version: 2023_11_20_185104) do
 
-   create_table "answers", force: :cascade do |t|
-     t.text "body"
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.text "body", null: false
     t.boolean "correct", default: false, null: false
-    t.integer "question_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "question_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
-  create_table "attempts", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "test_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["test_id"], name: "index_attempts_on_test_id"
-    t.index ["user_id"], name: "index_attempts_on_user_id"
-  end
+
   create_table "categories", force: :cascade do |t|
-    t.string "title", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.text "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
+  create_table "passed_tests", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "test_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["test_id"], name: "index_passed_tests_on_test_id"
+    t.index ["user_id"], name: "index_passed_tests_on_user_id"
+  end
+
   create_table "questions", force: :cascade do |t|
-    t.text "body"
-    t.integer "test_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.text "body", null: false
+    t.bigint "test_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["test_id"], name: "index_questions_on_test_id"
   end
+
   create_table "tests", force: :cascade do |t|
-     t.string "title", null: false
-     t.integer "level", default: 1, null: false
-     t.integer "category_id", null: false
-     t.integer "author_id"
-     t.datetime "created_at", precision: 6, null: false
-     t.datetime "updated_at", precision: 6, null: false
-     t.index ["author_id"], name: "index_tests_on_author_id"
+    t.text "name", null: false
+    t.integer "level", default: 0, null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_tests_on_author_id"
+    t.index ["category_id"], name: "index_tests_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.text "name", null: false
+    t.text "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "answers", "questions"
+  add_foreign_key "passed_tests", "tests"
+  add_foreign_key "passed_tests", "users"
+  add_foreign_key "questions", "tests"
+  add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users", column: "author_id"
+end
