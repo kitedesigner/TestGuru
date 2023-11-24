72c3589ed555b73139fb3d16af52558018ac23e3
kitedesigner <133687852+kitedesigner@users.noreply.github.com>
2023-11-24 09:55:22 -0800

    Add files via upload

---
+++ test.rb
@@ -0,0 +1,16 @@
+class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :author, class_name: 'User',
+  foreign_key: :author_id
+
+  has_many :questions, dependent: :destroy
+  has_many :attempts
+  has_many :users, through: :attempts
+
+  def self.find_by_category_name(name)
+    joins(:category)
+    .where(categories: { title: name })
+    .order(title: :desc)
+    .pluck(:title)
+  end
+end
---
+++ user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_many:authored_tests, class_name: 'Test', foreign_key: :author_id
+  has_many :attempts, dependent: :destroy
+  has_many :tests, through: :attempts
+
+  def find_tests_by_level(level)
+    tests.where(level: level).distinct
+  end
+end
