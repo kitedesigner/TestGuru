290f622fcb032a6b08a5d43e8105a0a7d2fa4727
kitedesigner <133687852+kitedesigner@users.noreply.github.com>
2023-11-24 00:52:40 -0800

    Update 20231120171845_set_null_true_to_tests_author_id.rb

--- db/migrate/20231120171845_set_null_true_to_tests_author_id.rb
+++ db/migrate/20231120171845_set_null_true_to_tests_author_id.rb
@@ -1,5 +1,5 @@
 class SetNullTrueToTestsAuthorId < ActiveRecord::Migration[6.1]
-   def change
-     change_column_null :tests, :author_id, true
-   end
- end
+def change 
+change_column_null :tests, :author_id, true
+end
+end
