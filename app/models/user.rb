abfbfb5de778408ec57b0564405981ddfb4ff0ce
kitedesigner <133687852+kitedesigner@users.noreply.github.com>
2023-11-24 10:03:20 -0800

    Add files via upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-has_many :authored_tests, foreign_key: :author_id, class_name: 'Test', dependent: :nullify
-has_many :attempts, dependent: :destroy
-has_many :tests, through: :attempts
+  has_many:authored_tests, class_name: 'Test', foreign_key: :author_id
+  has_many :attempts, dependent: :destroy
+  has_many :tests, through: :attempts
 
-def find_tests_by_level(level)
-tests.where(level: level).distinct
-end
+  def find_tests_by_level(level)
+    tests.where(level: level).distinct
+  end
 end
