481483faadcd118d9ef01f97697f2d7824439653
kitedesigner <133687852+kitedesigner@users.noreply.github.com>
2023-11-24 00:56:17 -0800

    Add files via upload

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+users = User.create!([
+                        {name: 'UserNo1', email: 'UserNo1@mail.ru'},
+                        {name: 'UserNo2', email: 'UserNo2@mail.ru'},
+                        {name: 'UserNo3', email: 'UserNo3@mail.ru'}
+                     ])
+
+categories = Category.create!([
+                                 {title: 'Web development'},
+                                 {title: 'Game development'},
+                                 {title: 'Software development'}
+                             ])
+
+tests = Test.create!([
+                        {name: 'Ruby on Rails', level: 3, category: categories[0]},
+                        {name: 'C++', level: 2, category: categories[1]},
+                        {name: 'Python', level: 1, category: categories[2]}
+                    ])
+
+questions = Question.create!([
+                                {body: 'To be, or not to be, that is the question_1_?', test: tests[0]},
+                                {body: 'To be, or not to be, that is the question_2_?', test: tests[1]},
+                                {body: 'To be, or not to be, that is the question_3_?', test: tests[2]}])
+
+answers = Answer.create!([
+                            {answer: 'yes', correct: true, question: questions[0]},
+                            {answer: 'no', question: questions[0]},
+                            {answer: 'maybe', question: questions[0]}
+                        ])
+
+passed_test = PassedTest.create!([
+                                    {user: users[0], test: tests[0]},
+                                    {user: users[0], test: tests[1]},
+                                    {user: users[0], test: tests[2]}
+                                ])
