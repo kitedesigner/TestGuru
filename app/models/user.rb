f9b96043cab5f7765c2374cfa30989aa7b6d3160
kitedesigner <133687852+kitedesigner@users.noreply.github.com>
2023-11-24 00:51:44 -0800

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-   has_many :authored_tests, foreign_key: :author_id, class_name: 'Test', dependent: :nullify
-   has_many :attempts, dependent: :destroy
-   has_many :tests, through: :attempts, dependent: :destroy
+has_many :authored_tests, foreign_key: :author_id, class_name: 'Test', dependent: :nullify
+has_many :attempts, dependent: :destroy
+has_many :tests, through: :attempts
 
-   def find_tests_by_level(level)
-     tests.where(level: level).distinct
-   end
- end
+def find_tests_by_level(level)
+tests.where(level: level).distinct
+end
+end
