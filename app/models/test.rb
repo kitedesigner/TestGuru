e48166bef55f073f1b18bd00e113c1e3d60d948f
kitedesigner <133687852+kitedesigner@users.noreply.github.com>
2023-11-28 12:43:36 -0800

    Update test.rb

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,15 +1,15 @@
-class Test < ApplicationRecord
-  belongs_to :category
-  belongs_to :author, class_name: 'User'
+  class Test < ApplicationRecord
+   belongs_to :category
+   belongs_to :author, class_name: 'User'
 
-  has_many :questions, dependent: :destroy
-  has_many :attempts
-  has_many :users, through: :attempts
+   has_many :questions, dependent: :destroy
+   has_many :attempts
+   has_many :users, through: :attempts
 
-  def self.find_by_category_name(name)
-    joins(:category)
-    .where(categories: { title: name })
-    .order(title: :desc)
-    .pluck(:title)
+    def self.find_by_category_name(name)
+     joins(:category)
+     .where(categories: { title: name })
+     .order(title: :desc)
+     .pluck(:title)
+   end
   end
-end
