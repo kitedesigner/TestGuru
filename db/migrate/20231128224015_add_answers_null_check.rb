7b1c7e509aa977a5423a429a3c90b243f381a091
kitedesigner <133687852+kitedesigner@users.noreply.github.com>
2023-11-28 13:13:46 -0800

    Add files via upload

---
+++ db/migrate/20231128224015_add_answers_null_check.rb
@@ -0,0 +1,7 @@
+class AddAnswersNullCheck < ActiveRecord::Migration[5.2]
+  def change
+    change_column_null(:answers, :body, false)
+    change_column_null(:answers, :correct, false)
+    change_column_null(:answers, :question_id, false)
+  end
+end
