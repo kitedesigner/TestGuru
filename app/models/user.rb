8e753f59cc5915d28f2b1db2500241bc69b23a4b
kitedesigner <133687852+kitedesigner@users.noreply.github.com>
2023-11-28 13:16:51 -0800

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
   class User < ApplicationRecord
     has_many :authored_tests, class_name: 'Test', foreign_key: :author_id
     has_many :passed_tests, dependent: :destroy
-    has_many :tests, through: :passed_tests, dependent: :destroy
+    has_many :tests, through: :passed_tests
 
     validates :email, presence: true
 
