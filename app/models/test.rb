997f71b3e590d22fdec74de42181b89954b7d02c
kitedesigner <133687852+kitedesigner@users.noreply.github.com>
2023-12-12 13:38:51 -0800

    Update test.rb

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,8 +3,8 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
   has_many :questions, dependent: :destroy
-  has_many :attempts, dependent: :destroy
-  has_many :users, through: :attempts, dependent: :destroy
+  has_many :attempts
+  has_many :users, through: :attempts
 
   validates :title, presence: true, uniqueness: { scope: :level }
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
