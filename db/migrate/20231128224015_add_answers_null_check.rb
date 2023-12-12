a89dfbe1e96996da88abfd17254e8e7fe5bec7d4
kitedesigner <133687852+kitedesigner@users.noreply.github.com>
2023-12-12 13:22:00 -0800

    Delete db/migrate/20231128224015_add_answers_null_check.rb

--- db/migrate/20231128224015_add_answers_null_check.rb
+++
@@ -1,7 +0,0 @@
-class AddAnswersNullCheck < ActiveRecord::Migration[5.2]
-  def change
-    change_column_null(:answers, :body, false)
-    change_column_null(:answers, :correct, false)
-    change_column_null(:answers, :question_id, false)
-  end
-end
